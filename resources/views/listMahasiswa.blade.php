926ecedef8295c604d6ba6f1717388c7241867af
Fiona CST <03081190001@student.uph.edu>
2022-04-01 18:35:50 +0700

    perubahan code + template + layout (sebelum adminLTE)

--- resources/views/listMahasiswa.blade.php
+++ resources/views/listMahasiswa.blade.php
@@ -1,28 +1,27 @@
-<html>
-    <head>
-        <title>List Profil Mahasiswa</title>
-        <style>
-            table, td, th {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-        </style>
-    </head>
-    
-    <body>
-        <table>
-            <tr>
-                <th>Nama</th>
-                <th>Jurusan</th>
-            </tr>
+@extends('template.mastertemplate')
 
-            @foreach ($mhs as $detailMhs)
-            <tr> <td>{{ $detailMhs['nama'] }}</td> 
-                <td>{{ $detailMhs['jurusan'] }}</td>
-            </tr>
-            @endforeach
-        </table>
-        
-        
-    </body>
-</html>
+@section('title', 'List Profil Mahasiswa')
+    
+@section('content')
+    <style>
+        table, td, th {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+
+    <h1>List Profil Mahasiswa</h1>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Jurusan</th>
+        </tr>
+
+        @foreach ($mhs as $detailMhs)
+        <tr> 
+            <td>{{ $detailMhs['nama'] }}</td> 
+            <td>{{ $detailMhs['jurusan'] }}</td>
+        </tr>
+        @endforeach
+    </table>
+@endsection
